05a628d96559bce37afd0d15e7cece32c1b904a7
Sam Pepler <sam.pepler@stfc.ac.uk>
2017-03-08 12:32:31 +0000

    rearrange for installable django app

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='cf-vocab-editor',
+    version='2.0',
+    packages=['vocab'],
+    include_package_data=True,
+    license='Open License',  # example license
+    description='A Django app to track versions of the climate and forcast (CF) standard name table.',
+    long_description=README,
+    url='http://www.ceda.ac.uk/',
+    author='Sam Pepler',
+    author_email='sam.pepler@stfc.ac.uk',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
