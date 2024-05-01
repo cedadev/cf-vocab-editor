54cf042e8e362c1304230a3fc430c63240e3847c
root <root@aharwood-docker-202309181416.ceda-dev-s.jasmin.ac.uk>
2024-05-01 15:12:22 +0100

    Updated for latest python and django

--- vocab/admin.py
+++ vocab/admin.py
@@ -1,52 +1,36 @@
 from django.contrib import admin
-# from django.contrib.gis import admin
+#from django.contrib.gis import admin
 from vocab.models import *
 from django import forms
 
 
+
 class ProposalAdmin(admin.ModelAdmin):
-    list_display = ('proposer', 'status', 'vocab_list_version')
-    # readonly_fields = ('status', 'vocab_list')
-
-
+   list_display = ('proposer', 'status', 'vocab_list_version')
+  # readonly_fields = ('status', 'vocab_list')
 admin.site.register(Proposal, ProposalAdmin)
 
-
 class TermAdmin(admin.ModelAdmin):
-    list_display = ('name', 'unit', 'externalid', 'proposals_links')
-    search_fields = ('name', 'externalid',)
-    list_filter = ('unit',)
-
-
+   list_display = ('name', 'unit',  'externalid', 'proposals_links' )
+   search_fields = ('name', 'externalid',)
+   list_filter = ('unit',)
 admin.site.register(Term, TermAdmin)
 
-
 class ProposedTermsAdmin(admin.ModelAdmin):
-    list_display = ('term', 'change_date', 'proposal')
-
-
+   list_display = ('term', 'change_date',  'proposal' )
 admin.site.register(ProposedTerms, ProposedTermsAdmin)
 
-
 class VocabListAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-
+   list_display = ('name',)
 admin.site.register(VocabList, VocabListAdmin)
 
-
 class AliasAdmin(admin.ModelAdmin):
-    list_display = ('name', 'termname')
-    search_fields = ('name', 'termname')
-
-
+   list_display = ('name','termname')
+   search_fields = ('name','termname')
 admin.site.register(Alias, AliasAdmin)
 
-
 class VocabListVersionAdmin(admin.ModelAdmin):
-    list_display = ('vocab_list', 'version',)
-
-
+   list_display = ('vocab_list', 'version',)
 admin.site.register(VocabListVersion, VocabListVersionAdmin)
 
 
@@ -58,11 +42,11 @@ class PhraseAdminForm(forms.ModelForm):
         }
         fields = {'regex', 'text'}
 
-
 class PhraseAdmin(admin.ModelAdmin):
-    form = PhraseAdminForm
-    list_display = ('regex', 'text')
-    search_fields = ('regex', 'text',)
-
-
+   form =PhraseAdminForm
+   list_display = ('regex', 'text')
+   search_fields = ('regex', 'text',)
 admin.site.register(Phrase, PhraseAdmin)
+
+
+
